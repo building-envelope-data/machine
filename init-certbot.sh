da2329c377f4bfbc1b2dd451f11565f05ae6996b
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-11-18 13:56:12 +0100

    Read domains and email address from dotenv file --- closes #6

--- init-certbot.sh
+++ init-certbot.sh
@@ -2,18 +2,23 @@
 
 # Inspired by https://github.com/wmnnd/nginx-certbot/blob/master/init-letsencrypt.sh
 
+# For the option `allexport` see https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -o allexport
+source .env
+set +o allexport
+
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
 
-domains=(buildingenvelopedata.org www.buildingenvelopedata.org staging.buildingenvelopedata.org)
-email="simon.wacker@ise.fraunhofer.de"
+domains=("${NON_WWW_PRODUCTION_HOST}" "${PRODUCTION_HOST}" "${STAGING_HOST}")
+email="${EMAIL_ADDRESS}"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "./certbot" ]; then
   read -p "Existing data found. Continue and replace existing certificate(s)? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+  if [ "${decision}" != "Y" ] && [ "${decision}" != "y" ]; then
     exit
   fi
 fi
@@ -26,33 +31,33 @@ if [ ! -e "./certbot/conf/options-ssl-nginx.conf" ] || [ ! -e "./certbot/conf/ss
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-mkdir -p "./certbot/conf/live/$domains"
-make OUT_PATH="/etc/letsencrypt/live/$domains" dummy-certificates
+echo "### Creating dummy certificate for ${domains} ..."
+mkdir -p "./certbot/conf/live/${domains}"
+make OUT_PATH="/etc/letsencrypt/live/${domains}" dummy-certificates
 echo
 
 echo "### (Re)deploying nginx ..."
 make deploy
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+echo "### Deleting dummy certificate for ${domains} ..."
 make DOMAINS="${domains}" delete-dummy-certificates
 echo
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
+echo "### Requesting Let's Encrypt certificate for ${domains} ..."
+#Join ${domains} to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
+  domain_args="${domain_args} -d ${domain}"
 done
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [ ${staging} != "0" ]; then staging_arg="--staging"; fi
 
 make \
-  STAGING_ARG="$staging_arg" \
-  DOMAIN_ARGS="$domain_args" \
-  EMAIL="$email" \
+  STAGING_ARG="${staging_arg}" \
+  DOMAIN_ARGS="${domain_args}" \
+  EMAIL="${email}" \
   request-certificates
 echo
 
