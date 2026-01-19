b9ca4a486377d4d747ca44dd728ab75456b3fe0b
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-01-19 12:49:37 +0100

    Harden the regular expression and pretty-print the output

--- monit_docker_healthcheck.sh
+++ monit_docker_healthcheck.sh
@@ -4,15 +4,16 @@ running_containers="$(docker ps --no-trunc --all --filter status=running --forma
 
 environment=machine
 services="$(make --silent --directory=/app/machine --file=/app/machine/Makefile list-services)"
-echo "Environment: '${environment}'"
-echo Services: ${services}
+echo "Docker Healthcheck"
+echo "  Environment: '${environment}'"
+echo "    Services: '${services}'"
 for service in ${services}
 do
   if [ "${service}" != "certbot" ] ; then
-    if echo "${running_containers}" | grep --quiet --extended-regexp "^${environment}-${service}-[0-9] [ a-zA-Z0-9]+ \(healthy\)$"
-      then echo "Service '${service}' is running and healthy"
+    if echo "${running_containers}" | grep --quiet --extended-regexp "^${environment}-${service}-[0-9] [ a-zA-Z0-9]+ \(healthy\)$" ; then
+      echo "    Service '${service}' is running and healthy"
     else
-      echo "Service '${service}' is not running and/or not healthy: $(docker ps --no-trunc --all --filter name="${environment}-${service}" --format '{{.ID}} {{.Image}} {{.Command}} {{.CreatedAt}} {{.Status}} {{.Ports}} {{.Names}}')" 1>&2
+      echo "    Service '${service}' is not running and/or not healthy: $(docker ps --no-trunc --all --filter name="${environment}-${service}" --format '{{.ID}} {{.Image}} {{.Command}} {{.CreatedAt}} {{.Status}} {{.Ports}} {{.Names}}')" 1>&2
       exit 1
     fi
   fi
@@ -20,16 +21,16 @@ done
 
 for environment in staging production
 do
-  echo "Environment: '${environment}'"
+  echo "  Environment: '${environment}'"
   services="$(make --silent --directory=/app/${environment} --file=/app/${environment}/Makefile.production list-services)"
-  echo Services: ${services}
+  echo "    Services: '${services}'"
   for service in ${services}
   do
-    echo "Service: '${service}'"
-    if echo "${running_containers}" | grep --quiet --extended-regexp "[a-zA-Z]+_${environment}-${service}-[0-9] [ a-zA-Z0-9]+ \(healthy\)"
-      then echo "Service '${service}' is running and healthy"
+    echo "    Service: '${service}'"
+    if echo "${running_containers}" | grep --quiet --extended-regexp "^[a-zA-Z]+_${environment}-${service}-[0-9] [ a-zA-Z0-9]+ \(healthy\)$" ; then
+      echo "    Service '${service}' is running and healthy"
     else
-      echo "Service '${service}' is not running and/or not healthy: $(docker ps --no-trunc --all --filter name="${environment}-${service}" --format '{{.ID}} {{.Image}} {{.Command}} {{.CreatedAt}} {{.Status}} {{.Ports}} {{.Names}}')" 1>&2
+      echo "    Service '${service}' is not running and/or not healthy: $(docker ps --no-trunc --all --filter name="${environment}-${service}" --format '{{.ID}} {{.Image}} {{.Command}} {{.CreatedAt}} {{.Status}} {{.Ports}} {{.Names}}')" 1>&2
       exit 1
     fi
   done
